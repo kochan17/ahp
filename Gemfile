784a272e70511e19efa5d216515bdf815b8a76b3
Kota <i7811832616@gmail.com>
2023-06-11 23:25:07 +0900

    OpenAI_API導入

--- Gemfile
+++ Gemfile
@@ -62,3 +62,4 @@ group :development do
   gem "rails-erd"
 end
 gem 'devise'
+gem 'ruby-openai'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,42 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+  end
+
+  def edit
+  end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
+      get_personality_recommendation
       redirect_to @user, notice: 'Profile was successfully updated.'
     else
       render :edit
     end
   end
-  
+
   private
-  
-  def user_params
-    params.require(:user).permit(:nickname, :age, :gender, /* 他のパラメータをここに追加... */)
+
+  def set_user
+    @user = User.find(params[:id])
   end
-  
+
+  def user_params
+    params.require(:user).permit(:nickname, :age, :gender)
+  end
+
+  def get_personality_recommendation
+    require 'openai'
+
+    OpenAI.api_key = ENV["OPENAI_API_KEY"]
+
+    response = OpenAI::Completion.create(
+      engine: "text-davinci-002",
+      prompt: "Based on the profile information: #{current_user.profile.interests}, the ideal personality traits in a partner would be...",
+      max_tokens: 100
+    )
+
+    current_user.profile.recommendation.update(text: response.choices.first.text.strip)
+  end
 end
